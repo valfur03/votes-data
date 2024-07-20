3a3496d4e74804545095b1937b037f38dd813ae6
valfur03 <valentin.furmanek@gmail.com>
2024-07-20 12:23:00 +0200

    feat: add background colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Votes Data",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#000091",
 };
 
 export default function RootLayout({

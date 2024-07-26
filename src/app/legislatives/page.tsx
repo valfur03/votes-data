558082fb1eda2970a7d390dd6bdf394571911ac4
valfur03 <valentin.furmanek@gmail.com>
2024-07-26 20:41:48 +0200

    feat: add DeputiesGrid to legislatives page

--- src/app/legislatives/page.tsx
+++ src/app/legislatives/page.tsx
@@ -1,3 +1,4 @@
+import { DeputiesGrid } from "@/components/deputies-grid/DeputiesGrid";
 import { FrenchHemicycle } from "@/components/french-hemicycle/FrenchHemicycle";
 import { TERM_0b5c63073d424f4c90cccc17ea29c4dd } from "@/data/legislatives/term/0b5c63073d424f4c90cccc17ea29c4dd";
 
@@ -12,6 +13,16 @@ export default function LegislativesPage() {
       <section className="px-safe rounded-b-lg bg-white">
         <div className="m-auto max-w-screen-lg p-2">
           <FrenchHemicycle term={TERM_0b5c63073d424f4c90cccc17ea29c4dd} />
+        </div>
+      </section>
+      <section className="px-safe">
+        <div className="m-auto max-w-screen-lg px-6 py-8">
+          <header className="mb-8 flex flex-row items-center gap-4 md:mb-12 md:mt-4 lg:mb-16 lg:mt-8 lg:gap-8">
+            <hr className="grow border-gray-400" />
+            <h2 className="text-gray-700">Liste des députés</h2>
+            <hr className="grow border-gray-400" />
+          </header>
+          <DeputiesGrid term={TERM_0b5c63073d424f4c90cccc17ea29c4dd} />
         </div>
       </section>
     </main>

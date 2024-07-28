532928f623efd34d19947b4ff4320276c33a032c
valfur03 <valentin.furmanek@gmail.com>
2024-07-28 15:07:11 +0200

    refactor: replace headers in hemicycle page with PageHeader

--- src/app/legislatives/hemicycle/page.tsx
+++ src/app/legislatives/hemicycle/page.tsx
@@ -1,20 +1,20 @@
 import { DeputiesGrid } from "@/components/deputies-grid/DeputiesGrid";
 import { FrenchHemicycle } from "@/components/french-hemicycle/FrenchHemicycle";
+import { PageHeader } from "@/components/page-header/PageHeader";
+import { PageHeaderSection } from "@/components/page-header/PageHeaderSection";
 import { TERM_0b5c63073d424f4c90cccc17ea29c4dd } from "@/data/legislatives/term/0b5c63073d424f4c90cccc17ea29c4dd";
 
 export default function LegislativesPage() {
   return (
     <main>
-      <section className="px-safe bg-white">
-        <div className="m-auto max-w-screen-lg px-6 py-8">
+      <PageHeader>
+        <PageHeaderSection className="m-auto max-w-screen-lg px-6 py-8">
           <h1>Élections législatives</h1>
-        </div>
-      </section>
-      <section className="px-safe rounded-b-lg bg-white">
-        <div className="m-auto max-w-screen-lg p-2">
+        </PageHeaderSection>
+        <PageHeaderSection className="m-auto max-w-screen-lg p-2">
           <FrenchHemicycle term={TERM_0b5c63073d424f4c90cccc17ea29c4dd} />
-        </div>
-      </section>
+        </PageHeaderSection>
+      </PageHeader>
       <section className="px-safe">
         <div className="m-auto max-w-screen-lg px-6 py-8">
           <header className="mb-8 flex flex-row items-center gap-4 md:mb-12 md:mt-4 lg:mb-16 lg:mt-8 lg:gap-8">

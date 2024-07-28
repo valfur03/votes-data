8318c2adcbaaee9dd8c99fe362d2e789813adf43
valfur03 <valentin.furmanek@gmail.com>
2024-07-28 15:35:08 +0200

    feat: add className property to HomeHeadline

--- src/components/home-headline/HomeHeadline.tsx
+++ src/components/home-headline/HomeHeadline.tsx
@@ -1,8 +1,19 @@
-export const HomeHeadline = () => {
+import { cn } from "@/lib/utils/cn";
+
+export type HomeHeadlineProps = {
+  className?: string;
+};
+
+export const HomeHeadline = ({ className }: HomeHeadlineProps) => {
   const highlightSpanClassName = "text-blue-france";
 
   return (
-    <p className="w-full max-w-[420px] text-4xl font-extrabold text-neutral-800">
+    <p
+      className={cn(
+        "w-full max-w-[420px] text-4xl font-extrabold text-neutral-800",
+        className,
+      )}
+    >
       Consulter les données des{" "}
       <span className={highlightSpanClassName}>élections</span> en France, de
       manière <span className={highlightSpanClassName}>claire</span> et{" "}

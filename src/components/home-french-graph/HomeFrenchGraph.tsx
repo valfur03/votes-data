4f1c0f129c70d88bd39d23bd3650ab8b03d897ca
valfur03 <valentin.furmanek@gmail.com>
2024-07-28 15:34:41 +0200

    feat: add className property to HomeFrenchGraph

--- src/components/home-french-graph/HomeFrenchGraph.tsx
+++ src/components/home-french-graph/HomeFrenchGraph.tsx
@@ -1,6 +1,17 @@
-export const HomeFrenchGraph = () => {
+import { cn } from "@/lib/utils/cn";
+
+export type HomeFrenchGraphProps = {
+  className?: string;
+};
+
+export const HomeFrenchGraph = ({ className }: HomeFrenchGraphProps) => {
   return (
-    <div className="flex h-48 items-end gap-x-2.5 [&>div]:box-border [&>div]:w-12 [&>div]:rounded [&>div]:border-2">
+    <div
+      className={cn(
+        "flex h-48 items-end gap-x-2.5 [&>div]:box-border [&>div]:w-12 [&>div]:rounded [&>div]:border-2",
+        className,
+      )}
+    >
       <div className="h-full border-indigo-200 bg-indigo-100" />
       <div className="h-16 border-neutral-200 bg-neutral-100" />
       <div className="h-40 border-red-200 bg-red-100" />

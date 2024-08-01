e4a76bb889205ddfb578739e287217bcb6880a09
valfur03 <valentin.furmanek@gmail.com>
2024-08-01 19:47:07 +0200

    feat: add ElectionTimelineSection to the legislatives page

--- src/app/legislatives/page.tsx
+++ src/app/legislatives/page.tsx
@@ -1,13 +1,13 @@
 import { PageContent } from "@/components/page-content/PageContent";
 import { PageHeader } from "@/components/page-header/PageHeader";
-import { PageHeaderSection } from "@/components/page-header/PageHeaderSection";
 import { DatavizCardsSection } from "@/sections/legislatives/dataviz-cards.section";
+import { ElectionTimelineSection } from "@/sections/legislatives/election-timeline.section";
 
 export default function LegislativesPage() {
   return (
     <>
       <PageHeader>
-        <PageHeaderSection className="flex p-8"></PageHeaderSection>
+        <ElectionTimelineSection />
       </PageHeader>
       <PageContent>
         <DatavizCardsSection />

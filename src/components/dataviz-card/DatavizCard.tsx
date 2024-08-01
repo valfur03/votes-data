6fbfb0e360579ff925d9d4b6f8e08c1e54e88aa9
valfur03 <valentin.furmanek@gmail.com>
2024-08-01 18:28:22 +0200

    feat: add DatavizCard components

---
+++ src/components/dataviz-card/DatavizCard.tsx
@@ -0,0 +1,71 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/card/Card";
+import { cn } from "@/lib/utils/cn";
+import * as React from "react";
+
+const DatavizCard = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <Card ref={ref} className={cn(className)} {...props} />
+));
+DatavizCard.displayName = "DatavizCard";
+
+const DatavizCardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <CardHeader ref={ref} className={cn("max-w-[75%]", className)} {...props} />
+));
+DatavizCardHeader.displayName = "DatavizCardHeader";
+
+const DatavizCardTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <CardTitle ref={ref} className={cn(className)} {...props} />
+));
+DatavizCardTitle.displayName = "DatavizCardTitle";
+
+const DatavizCardDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <CardDescription ref={ref} className={cn(className)} {...props} />
+));
+DatavizCardDescription.displayName = "DatavizCardDescription";
+
+const DatavizCardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <CardContent ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+));
+DatavizCardContent.displayName = "DatavizCardContent";
+
+const DatavizCardFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <CardFooter
+    ref={ref}
+    className={cn("justify-end justify-self-end", className)}
+    {...props}
+  />
+));
+DatavizCardFooter.displayName = "DatavizCardFooter";
+
+export {
+  DatavizCard,
+  DatavizCardHeader,
+  DatavizCardFooter,
+  DatavizCardTitle,
+  DatavizCardDescription,
+  DatavizCardContent,
+};

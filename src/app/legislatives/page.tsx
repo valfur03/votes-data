455f5cf7d95df988713773f70b960c0f773c3f74
valfur03 <valentin.furmanek@gmail.com>
2024-08-01 18:28:32 +0200

    feat: add add DatavizCard for the hemicycle page

--- src/app/legislatives/page.tsx
+++ src/app/legislatives/page.tsx
@@ -1,3 +1,45 @@
+import {
+  DatavizCard,
+  DatavizCardContent,
+  DatavizCardFooter,
+  DatavizCardHeader,
+  DatavizCardTitle,
+} from "@/components/dataviz-card/DatavizCard";
+import { PageContent } from "@/components/page-content/PageContent";
+import { PageContentSection } from "@/components/page-content/PageContentSection";
+import { PageHeader } from "@/components/page-header/PageHeader";
+import { PageHeaderSection } from "@/components/page-header/PageHeaderSection";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+
 export default function LegislativesPage() {
-  return <></>;
+  return (
+    <>
+      <PageHeader>
+        <PageHeaderSection className="flex p-8"></PageHeaderSection>
+      </PageHeader>
+      <PageContent>
+        <PageContentSection className="grid w-fit grid-cols-1 gap-8 p-8 lg:grid-cols-[repeat(2,448px)]">
+          <Link href="/legislatives/hemicycle">
+            <DatavizCard>
+              <DatavizCardHeader>
+                <DatavizCardTitle>
+                  Composition de l&apos;hémicycle
+                </DatavizCardTitle>
+              </DatavizCardHeader>
+              <DatavizCardContent>
+                <p>
+                  Repérez le siège de vos députés et visualisez la composition
+                  de l&apos;assemblée par groupe.
+                </p>
+              </DatavizCardContent>
+              <DatavizCardFooter>
+                <ArrowRightIcon className="size-6" />
+              </DatavizCardFooter>
+            </DatavizCard>
+          </Link>
+        </PageContentSection>
+      </PageContent>
+    </>
+  );
 }

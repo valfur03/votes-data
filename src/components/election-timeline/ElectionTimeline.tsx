c65dea5558bf1c95a93b363b295e8e8b7dd0d7ce
valfur03 <valentin.furmanek@gmail.com>
2024-08-01 20:01:46 +0200

    feat: style the next election circle with border and inner circle

--- src/components/election-timeline/ElectionTimeline.tsx
+++ src/components/election-timeline/ElectionTimeline.tsx
@@ -9,7 +9,7 @@ export const ElectionTimeline = () => {
   return (
     <div className="relative w-[calc(50%+48px+190px/2)]">
       <div className="relative z-10 flex items-end justify-between px-12">
-        <div className="flex w-full max-w-14 flex-col items-center gap-1 text-center">
+        <div className="mb-1 flex w-full max-w-14 flex-col items-center gap-1 text-center">
           <div className="w-full">
             <p className="text-xs text-neutral-700">12 & 19 juin 2022</p>
           </div>
@@ -27,10 +27,12 @@ export const ElectionTimeline = () => {
               <p>... dans 678 jours</p>
             </CardContent>
           </Card>
-          <div className="size-6 rounded-full bg-blue-france"></div>
+          <div className="box-border flex size-8 items-center justify-center rounded-full border-4 border-blue-france bg-white">
+            <div className="size-4 rounded-full bg-blue-france"></div>
+          </div>
         </div>
       </div>
-      <div className="absolute bottom-[8px] h-2 w-[calc(100%-48px-190px/2)] rounded-full bg-gradient-to-r from-transparent via-indigo-100 via-[72px] to-blue-france to-[calc(100%-72px)]"></div>
+      <div className="absolute bottom-[12px] h-2 w-[calc(100%-48px-190px/2)] rounded-full bg-gradient-to-r from-transparent via-indigo-100 via-[72px] to-blue-france to-[calc(100%-72px)]"></div>
     </div>
   );
 };

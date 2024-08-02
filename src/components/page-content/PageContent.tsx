f6d163777ee7976b73eab684585a78d966fd7f45
valfur03 <valentin.furmanek@gmail.com>
2024-08-02 21:35:06 +0200

    feat: add className property to PageContent

--- src/components/page-content/PageContent.tsx
+++ src/components/page-content/PageContent.tsx
@@ -1,7 +1,10 @@
+import { cn } from "@/lib/utils/cn";
 import { PropsWithChildren } from "react";
 
-export type PageContentProps = PropsWithChildren;
+export type PageContentProps = PropsWithChildren<{
+  className?: string;
+}>;
 
-export const PageContent = ({ children }: PageContentProps) => {
-  return <div>{children}</div>;
+export const PageContent = ({ children, className }: PageContentProps) => {
+  return <div className={cn(className)}>{children}</div>;
 };

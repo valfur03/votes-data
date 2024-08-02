1ac52807cd6aad9a6fe6d639ab8cef225e6a5019
valfur03 <valentin.furmanek@gmail.com>
2024-08-02 21:38:00 +0200

    feat: add a title to the legislatives page

--- src/app/legislatives/page.tsx
+++ src/app/legislatives/page.tsx
@@ -1,5 +1,6 @@
 import { PageContent } from "@/components/page-content/PageContent";
 import { PageHeader } from "@/components/page-header/PageHeader";
+import { PageHeaderSection } from "@/components/page-header/PageHeaderSection";
 import { DatavizCardsSection } from "@/sections/legislatives/dataviz-cards.section";
 import { ElectionTimelineSection } from "@/sections/legislatives/election-timeline.section";
 
@@ -7,6 +8,12 @@ export default function LegislativesPage() {
   return (
     <>
       <PageHeader>
+        <PageHeaderSection
+          beforeReturn={true}
+          className="m-auto max-w-screen-lg p-6 pb-0"
+        >
+          <h1>Élections législatives</h1>
+        </PageHeaderSection>
         <ElectionTimelineSection />
       </PageHeader>
       <PageContent>

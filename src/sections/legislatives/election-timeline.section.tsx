e19c5525c5cdd84b7eb753b66d43aecf2b3c0a5c
valfur03 <valentin.furmanek@gmail.com>
2024-08-02 21:36:39 +0200

    feat: prevent overriding vertical padding starting sm devices

--- src/sections/legislatives/election-timeline.section.tsx
+++ src/sections/legislatives/election-timeline.section.tsx
@@ -3,7 +3,7 @@ import { PageHeaderSection } from "@/components/page-header/PageHeaderSection";
 
 export const ElectionTimelineSection = () => {
   return (
-    <PageHeaderSection className="flex justify-start py-8 sm:p-[--header-section-padding-x]">
+    <PageHeaderSection className="flex justify-start py-8 sm:px-[--header-section-padding-x]">
       <ElectionTimeline />
     </PageHeaderSection>
   );

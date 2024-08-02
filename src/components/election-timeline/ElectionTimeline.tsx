30e89a3f5501730d535f74976c041cc6d87b766d
valfur03 <valentin.furmanek@gmail.com>
2024-08-02 09:00:21 +0200

    feat: display the election timeline horizontally on mobile devices

--- src/components/election-timeline/ElectionTimeline.tsx
+++ src/components/election-timeline/ElectionTimeline.tsx
@@ -7,16 +7,16 @@ import {
 
 export const ElectionTimeline = () => {
   return (
-    <div className="relative w-fit sm:w-[calc(50%+3rem+12rem/2)]">
-      <div className="relative z-10 flex flex-col-reverse justify-end gap-y-24 py-4 sm:flex-row sm:items-end sm:justify-between sm:gap-y-0 sm:px-12 sm:py-0">
-        <div className="ml-1 flex w-full max-w-24 flex-row-reverse items-center gap-1 text-center sm:mb-1 sm:ml-0 sm:max-w-14 sm:flex-col">
+    <div className="relative w-full max-w-[calc(50%+3rem+12rem/2)]">
+      <div className="relative z-10 flex flex-row items-end justify-between pl-[calc(1.5rem+env(safe-area-inset-left))] pr-6 sm:px-12">
+        <div className="mb-1 flex w-full max-w-14 flex-col items-center gap-1 text-center">
           <div className="w-full">
             <p className="text-xs text-neutral-700">12 & 19 juin 2022</p>
           </div>
           <div className="size-6 shrink-0 rounded-full bg-indigo-100"></div>
         </div>
-        <div className="flex w-full flex-row-reverse items-center justify-end gap-2 text-center sm:max-w-48 sm:flex-col">
-          <Card className="h-[114px] gap-2">
+        <div className="flex w-full max-w-48 flex-col items-center gap-2 text-center">
+          <Card className="gap-2">
             <CardHeader className="pb-0">
               <CardTitle className="whitespace-pre-line text-base font-semibold text-neutral-800">
                 30 juin & {"\n"}
@@ -32,7 +32,7 @@ export const ElectionTimeline = () => {
           </div>
         </div>
       </div>
-      <div className="absolute bottom-0 left-[10px] h-[calc(100%-1rem-114px/2)] w-2 rounded-full bg-gradient-to-t from-transparent via-indigo-100 via-[24px] to-blue-france to-[calc(100%-24px)] sm:bottom-[12px] sm:top-[revert] sm:h-2 sm:w-[calc(100%-3rem-12rem/2)] sm:bg-gradient-to-r sm:via-[72px] sm:to-[calc(100%-72px)]" />
+      <div className="absolute bottom-[12px] top-[revert] h-2 w-[calc(100%-1.5rem-12rem/2)] rounded-full bg-gradient-to-r from-transparent via-indigo-100 via-[calc(1.5rem*2)] to-blue-france to-[calc(100%-1.5rem)] sm:w-[calc(100%-3rem-12rem/2)] sm:via-[72px] sm:to-[calc(100%-72px)]" />
     </div>
   );
 };
--- src/sections/legislatives/election-timeline.section.tsx
+++ src/sections/legislatives/election-timeline.section.tsx
@@ -3,7 +3,7 @@ import { PageHeaderSection } from "@/components/page-header/PageHeaderSection";
 
 export const ElectionTimelineSection = () => {
   return (
-    <PageHeaderSection className="flex justify-center p-8 sm:justify-start">
+    <PageHeaderSection className="flex justify-start py-8 sm:p-8">
       <ElectionTimeline />
     </PageHeaderSection>
   );

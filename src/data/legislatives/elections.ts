6db3b00d4d748a44356cc1c6e96da475e5b6e49b
valfur03 <valentin.furmanek@gmail.com>
2024-08-03 12:56:07 +0200

    feat: add potential next legislatives elections

--- src/data/legislatives/elections.ts
+++ src/data/legislatives/elections.ts
@@ -2,6 +2,10 @@ import { Election } from "@/types/election";
 
 export const LEGISLATIVES_ELECTIONS: [Election, Election, ...Election[]] = [
   {
+    firstRoundDate: new Date("2029-07-01"),
+    secondRoundDate: new Date("2029-07-08"),
+  },
+  {
     firstRoundDate: new Date("2024-06-30"),
     secondRoundDate: new Date("2024-07-07"),
   },

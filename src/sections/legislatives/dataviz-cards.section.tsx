f643353e6589693ef0b6d07c1cd7efa2115649ed
valfur03 <valentin.furmanek@gmail.com>
2024-08-04 18:17:49 +0200

    feat: add the FrenchHemicyclePie as the CardLink's banner

--- src/sections/legislatives/dataviz-cards.section.tsx
+++ src/sections/legislatives/dataviz-cards.section.tsx
@@ -1,12 +1,15 @@
 import {
   DatavizCard,
+  DatavizCardBanner,
   DatavizCardContent,
   DatavizCardFooter,
   DatavizCardHeader,
   DatavizCardTitle,
 } from "@/components/dataviz-card/DatavizCard";
+import { FrenchHemicyclePie } from "@/components/french-hemicycle-pie/FrenchHemicyclePie";
 import { PageContentCardsGridSection } from "@/components/page-content/PageContentCardsGridSection";
 import { DATAVIZ_PAGES } from "@/data/legislatives/dataviz/pages";
+import { TERM_0b5c63073d424f4c90cccc17ea29c4dd } from "@/data/legislatives/term/0b5c63073d424f4c90cccc17ea29c4dd";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 
@@ -16,6 +19,11 @@ export const DatavizCardsSection = () => {
       {DATAVIZ_PAGES.map(({ title, description, href }) => (
         <Link href={href} key={`${title}-${href}`}>
           <DatavizCard>
+            <DatavizCardBanner className="p-6">
+              <FrenchHemicyclePie
+                term={TERM_0b5c63073d424f4c90cccc17ea29c4dd}
+              />
+            </DatavizCardBanner>
             <DatavizCardHeader>
               <DatavizCardTitle>{title}</DatavizCardTitle>
             </DatavizCardHeader>

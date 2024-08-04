90acfe0e2791e750cc5c638e0347e8ebc27179f4
valfur03 <valentin.furmanek@gmail.com>
2024-08-04 12:57:11 +0200

    feat: add QueryTabs component

---
+++ src/components/query-tabs/QueryTabs.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Tabs } from "@/components/tabs/Tabs";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { PropsWithChildren } from "react";
+
+export type QueryTabsProps = PropsWithChildren<
+  React.ComponentPropsWithoutRef<typeof Tabs>
+>;
+
+export const TAB_INITIAL_VALUE_SP_KEY = "tab";
+
+export const QueryTabs = ({
+  children,
+  onValueChange,
+  value,
+  ...props
+}: QueryTabsProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const handleValueChange = (value: string) => {
+    const updatedSearchParams = new URLSearchParams(searchParams);
+    updatedSearchParams.set(TAB_INITIAL_VALUE_SP_KEY, value);
+
+    router.push(`${pathname}?${updatedSearchParams}`);
+
+    return onValueChange?.(value);
+  };
+
+  return (
+    <Tabs value={value} {...props} onValueChange={handleValueChange}>
+      {children}
+    </Tabs>
+  );
+};

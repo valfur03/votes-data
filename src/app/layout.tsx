a7e33be466225926dbfd50dce3fc4b3409bc2c57
valfur03 <valentin.furmanek@gmail.com>
2024-08-07 19:49:31 +0200

    feat: add title and description meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,9 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Votes Data",
+  title: "Votes Data – L'intégralité des data des élections en France",
+  description:
+    "Votes Data permet de consulter les données liées aux élections en France, en l'occurrence présidentielles, législatives ou municipales.",
 };
 
 export const viewport: Viewport = {

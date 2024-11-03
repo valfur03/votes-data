43459f6a4e1f07de7b188ffff8e78486f5b5636a
valfur03 <valentin.furmanek@gmail.com>
2024-11-03 12:07:12 +0100

    refactor: use Divider component in place of <hr />

--- src/app/legislatives/hemicycle/page.tsx
+++ src/app/legislatives/hemicycle/page.tsx
@@ -1,4 +1,5 @@
 import { DeputiesGrid } from "@/components/deputies-grid/DeputiesGrid";
+import { Divider } from "@/components/divider/Divider";
 import { LegislativesElectionGroupsScoresGrid } from "@/components/legislatives-election-group-score/LegislativesElectionGroupsScoresGrid";
 import { PageHeader } from "@/components/page-header/PageHeader";
 import { PageHeaderSection } from "@/components/page-header/PageHeaderSection";
@@ -28,9 +29,9 @@ export default function LegislativesHemicyclePage({
       <section className="px-safe">
         <div className="m-auto max-w-screen-lg px-6 py-8">
           <header className="mb-8 flex flex-row items-center gap-4 md:mb-12 md:mt-4 lg:mb-16 lg:mt-8 lg:gap-8">
-            <hr className="grow border-gray-400" />
+            <Divider />
             <h2 className="text-gray-700">Liste des députés</h2>
-            <hr className="grow border-gray-400" />
+            <Divider />
           </header>
           <DeputiesGrid term={TERM_0b5c63073d424f4c90cccc17ea29c4dd} />
         </div>

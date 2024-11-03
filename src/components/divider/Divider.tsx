d0bf8f1cef85c51117ce24fe4e6091ade85fd305
valfur03 <valentin.furmanek@gmail.com>
2024-11-03 12:18:20 +0100

    feat: add className to Divider

--- src/components/divider/Divider.tsx
+++ src/components/divider/Divider.tsx
@@ -1,3 +1,8 @@
-export const Divider = () => {
-  return <hr className="grow border-gray-400" />;
+import clsx from "clsx";
+import React from "react";
+
+export type DividerProps = React.HTMLAttributes<HTMLHRElement>;
+
+export const Divider = ({ className, ...props }: DividerProps) => {
+  return <hr className={clsx("grow border-gray-400", className)} {...props} />;
 };

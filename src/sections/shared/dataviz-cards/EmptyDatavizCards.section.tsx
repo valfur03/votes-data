a90fde4309687c37cb71048360c68fb7bbe16385
valfur03 <valentin.furmanek@gmail.com>
2024-11-03 13:14:18 +0100

    feat: improve mobile version of EmptyDatavizCards

--- src/sections/shared/dataviz-cards/EmptyDatavizCards.section.tsx
+++ src/sections/shared/dataviz-cards/EmptyDatavizCards.section.tsx
@@ -3,11 +3,11 @@ import { PageContentSection } from "@/components/page-content/PageContentSection
 
 export const EmptyDatavizCardsSection = () => {
   return (
-    <PageContentSection className="gap-8 py-16 text-center">
+    <PageContentSection className="gap-8 py-16 text-center max-md:max-w-80">
       <p className="mb-8 text-3xl font-semibold text-neutral-700">
         Des données arrivent bientôt ici...
       </p>
-      <Divider className="mx-auto max-w-96" />
+      <Divider className="mx-auto max-w-96 max-md:max-w-28" />
     </PageContentSection>
   );
 };

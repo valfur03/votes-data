dd3729e4445654de5ea92734bb7ec5a6533d0b6d
valfur03 <valentin.furmanek@gmail.com>
2024-11-07 13:16:05 +0100

    refactor: use TitleDivider in hemicycle page

--- src/app/legislatives/hemicycle/page.tsx
+++ src/app/legislatives/hemicycle/page.tsx
@@ -1,10 +1,10 @@
 import { DeputiesGrid } from "@/components/deputies-grid/DeputiesGrid";
-import { Divider } from "@/components/divider/Divider";
 import { LegislativesElectionGroupsScoresGrid } from "@/components/legislatives-election-group-score/LegislativesElectionGroupsScoresGrid";
 import { PageHeader } from "@/components/page-header/PageHeader";
 import { PageHeaderSection } from "@/components/page-header/PageHeaderSection";
 import { PageHeaderTitle } from "@/components/page-header/PageHeaderTitle";
 import { TAB_INITIAL_VALUE_SP_KEY } from "@/components/query-tabs/shared/constants/tab-initial-value-sp-key";
+import { TitleDivider } from "@/components/title-divider/TitleDivider";
 import { LEGISLATIVES_HEMICYCLE_PAGE_TITLE } from "@/constant/pages-title";
 import { TERM_0b5c63073d424f4c90cccc17ea29c4dd } from "@/data/legislatives/term/0b5c63073d424f4c90cccc17ea29c4dd";
 import { LegislativesHemicycleHeaderTabsSection } from "@/sections/legislatives/hemicycle/LegislativesHemicycleHeaderTabs.section";
@@ -28,11 +28,7 @@ export default function LegislativesHemicyclePage({
       </PageHeader>
       <section className="px-safe">
         <div className="m-auto max-w-screen-lg px-6 py-8">
-          <header className="mb-8 flex flex-row items-center gap-4 md:mb-12 md:mt-4 lg:mb-16 lg:mt-8 lg:gap-8">
-            <Divider className="grow" />
-            <h2 className="text-gray-700">Liste des députés</h2>
-            <Divider className="grow" />
-          </header>
+          <TitleDivider>Liste des députés</TitleDivider>
           <DeputiesGrid term={TERM_0b5c63073d424f4c90cccc17ea29c4dd} />
         </div>
       </section>

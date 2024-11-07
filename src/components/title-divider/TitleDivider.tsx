7e08b9dba98384f0f769b2d2576c1201f704d9dd
valfur03 <valentin.furmanek@gmail.com>
2024-11-07 13:15:47 +0100

    feat: add TitleDivider component

---
+++ src/components/title-divider/TitleDivider.tsx
@@ -0,0 +1,14 @@
+import { Divider } from "@/components/divider/Divider";
+import { PropsWithChildren } from "react";
+
+export type TitleDividerPrpos = PropsWithChildren;
+
+export const TitleDivider = ({ children }: TitleDividerPrpos) => {
+  return (
+    <header className="mb-8 flex flex-row items-center gap-4 md:mb-12 md:mt-4 lg:mb-16 lg:mt-8 lg:gap-8">
+      <Divider className="grow" />
+      <h2 className="text-gray-700">{children}</h2>
+      <Divider className="grow" />
+    </header>
+  );
+};
